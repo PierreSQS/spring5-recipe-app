c20294c9a2dc5c04a0413f96ace3726d70150fe2
PierreSQS <nomo_messina@web.de>
2021-05-23 19:09:21 +0200

    Chap175: Revisit. Assignment Write Test for IndexController

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,6 +1,5 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,11 +7,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
 public class IndexControllerTest {
 
@@ -27,19 +23,18 @@ public class IndexControllerTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        indexController = new IndexController(recipeSrv);
     }
 
     @Test
     public void getIndexPage() {
-        Set<Recipe> recipes = new HashSet<>();
-        Recipe recipe1 = new Recipe();
-        recipe1.setDescription("Recipe1");
-        recipes.add(recipe1);
-        when(recipeSrv.getRecipes()).thenReturn(recipes);
+        String aString = "index";
+        indexController = new IndexController(recipeSrv);
 
         String indexPage = indexController.getIndexPage(model);
-        assertEquals("index",indexPage);
-        verify(model,times(1)).addAttribute("recipes",recipeSrv.getRecipes());
+
+        verify(recipeSrv).getRecipes();
+        verify(model).addAttribute("recipes", recipeSrv.getRecipes());
+
+        assertEquals(aString,indexPage);
     }
 }
