fb5fe905e0a552f70fc45c7b550e28221867d917
PierreSQS <nomo_messina@web.de>
2021-07-17 17:44:45 +0200

    adding test with Inject-Annotation - NOK

--- src/test/java/guru/springframework/controllers/IngredientControllerInjectAnnotationTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerInjectAnnotationTest.java
@@ -7,6 +7,7 @@ import guru.springframework.services.RecipeService;
 import guru.springframework.services.UnitOfMeasureService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
@@ -33,13 +34,13 @@ public class IngredientControllerInjectAnnotationTest {
     @Mock
     RecipeService recipeService;
 
+    @InjectMocks
     IngredientController controller;
 
     MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
 
         controller = new IngredientController(ingredientService, recipeService, unitOfMeasureService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
