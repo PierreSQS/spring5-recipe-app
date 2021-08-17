69b809235d3ec9b0bd1873450c0a2421b3001503
PierreSQS <nomo_messina@web.de>
2021-08-17 14:06:44 +0200

    Chap234: Extended Test for failing Validation Path

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -17,12 +17,15 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
- * Created by jt on 6/19/17.
+ * modified by pierrot on 8/17/21.
  */
 public class RecipeControllerTest {
+
+    public static final String RECIPEFORM = "recipe/recipeform";
 
     @Mock
     RecipeService recipeService;
@@ -78,7 +81,7 @@ public class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/new"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(view().name(RECIPEFORM))
                 .andExpect(model().attributeExists("recipe"));
     }
 
@@ -100,6 +103,25 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .param("id", "")
+                    .param("description", "some string"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(model().attributeHasErrors())
+                .andExpect(model().attributeHasFieldErrors("recipe","directions"))
+                .andExpect(view().name(RECIPEFORM))
+                .andDo(print());
+    }
+
+    @Test
     public void testGetUpdateView() throws Exception {
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
@@ -108,7 +130,7 @@ public class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/1/update"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(view().name(RECIPEFORM))
                 .andExpect(model().attributeExists("recipe"));
     }
 
